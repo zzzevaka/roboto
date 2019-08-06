eaea79cbb32abb6ac8a2a3a267bb11da673d24ea
Максимов Леонид <l.maksimov@corp.mail.ru>
2019-08-06 21:03:16 +0300

    finam

--- finam_data/models.py
+++ finam_data/models.py
@@ -1,13 +1,32 @@
 from roboto.models import AbstractInstrument, AbstractCandle
 from django.db import models
-from finam.export import Exporter, Market, LookupComparator, Timeframe
+
+
+class ExtendedMarket(object):
+    WORLD_CURRENCIES = 5
+    MOSCOW_CURRENCIES = 45
+
+    # from finam.export.Market
+    BONDS = 2
+    COMMODITIES = 24
+    CURRENCIES = 45
+    ETF = 28
+    ETF_MOEX = 515
+    FUTURES = 14  # non-expired futures
+    FUTURES_ARCHIVE = 17  # expired futures
+    FUTURES_USA = 7
+    INDEXES = 6
+    SHARES = 1
+    SPB = 517
+    USA = 25
 
 
 class Instrument(AbstractInstrument):
     MARKET_CURRENCIES = 0
 
     MARKET_CHOICES = (
-        (MARKET_CURRENCIES, 'Currencies'),
+        (ExtendedMarket.MOSCOW_CURRENCIES, 'Moscow currencies'),
+        (ExtendedMarket.WORLD_CURRENCIES, 'World currencies'),
     )
 
     market = models.SmallIntegerField(choices=MARKET_CHOICES)
